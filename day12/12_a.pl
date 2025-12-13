c814df3de5c537d20f04e6feaca90f9266eae2f4
chatoix1992 <jchaotixh@gmail.com>
2025-12-13 14:27:01 +0100

    day 12

---
+++ day12/12_a.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my $fn = "data";
+$fn = "test" if $ENV{TEST};
+
+open(my $FH, '<', $fn) or die("Could not open file $fn: " . $!);
+
+chomp(my @lines = <$FH>);
+close($FH);
+
+# Get the presents
+my @presents;
